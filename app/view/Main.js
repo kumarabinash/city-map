25e15259b305be6f20cdf1bbcb0249ed4ea9a17c
Kumar Abinash <abinash.b2@gmail.com>
2014-03-18 03:45:06 +0530

    Added new map tab/page. Map page shows current location. Google maps api used.

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,7 +3,7 @@ Ext.define('CityMap.view.Main', {
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.Map'
     ],
     config: {
         tabBarPosition: 'bottom',
@@ -19,29 +19,30 @@ Ext.define('CityMap.view.Main', {
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                    title: 'CityMap'
                 },
 
                 html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
+                    "<h1>CityMap lets you see the map of your city and whats going on it your city on a map.</h1>",
+                    "<h2>Please click on the CityMap tab to search for your city</h2>",
+                    "Please allow CityMap to access your current location to show you the map of your current city."
                 ].join("")
             },
             {
-                title: 'Get Started',
+                title: 'CityMap',
                 iconCls: 'action',
+                xtype: 'map',
+                useCurrentLocation: true,
+                mapOptions: {
+                    zoom: 13,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
 
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        title: 'Map of your city'
                     }
                 ]
             }
